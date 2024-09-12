488dbf3ae6e97bf33d6b2684778e5fad97a4b8b8
KJG19 <kyleguenter19@gmail.com>
2024-09-12 13:21:59 -0600

    Completed Week 2 Assignment
    Added link and header to home page. Added Shopping List header and Student Info component to week 2 page. Created a new page for student info component.

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,9 +1,11 @@
+import StudentInfo from "./student-info";
 
 
-export default function Page(){
+export default function WeekTwoPage(){
     return(
         <main>
-            
+            <h1>Shopping List</h1> 
+            <StudentInfo />
         </main>
 
     );
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function StudentInfo(){
+    return(
+        <div>
+            <h3>Kyle Guenter</h3>
+            <Link href="https://github.com/KJG19/cprg306-assignments">https://github.com</Link>
+        </div>
+    );
+}
