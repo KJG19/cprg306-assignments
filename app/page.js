63dd15fd21fd444b908d330480e41f0b5662e5c3
KJG19 <kyleguenter19@gmail.com>
2024-09-24 19:47:41 -0600

    Created Week 3 Page
    Added week 3 page with shopping list and style with tailwind css.

--- app/page.js
+++ app/page.js
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG: 306 Web Development 2 - Assignments</h1>
+      <div>
       <Link href="./week-2">Week 2 Assignment</Link>
+      </div>
+      <div>
+      <Link href="./week-3">Week 3 Assignment</Link>
+      </div>
+      
 
     </main>
   );
